afc279966274ebd7eb388cdc05bb74c142dab604
mdl <codermdl@sina.com>
2018-04-16 11:13:39 +0800

    增加新功能模块

---
+++ test/newfeature/newfeature1.swift
@@ -0,0 +1,14 @@
+//
+//  newfeature1.swift
+//  test
+//
+//  Created by dongli mei on 2018/4/16.
+//  Copyright © 2018年 dongli mei. All rights reserved.
+//
+
+import Foundation
+class newFeature {
+    func fun1() {
+        print("feature 1")
+    }
+}
