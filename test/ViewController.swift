2588c86ac8e33ad80bb4d79e75e2ea9b2133acb3
mdl <codermdl@sina.com>
2018-04-16 10:31:49 +0800

    修改了1个bu'g

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("developer 1")
+        print("fix bug 1")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
